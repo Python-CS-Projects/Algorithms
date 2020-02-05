a9d012cce424c7f0926a30d59e24cfce568aa82a
fritzgt <tuanis@live.com>
2020-02-05 04:21:29 +0200

    initial commit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,6 +3,13 @@
 import argparse
 
 def find_max_profit(prices):
+  #takes in a list of stock prices
+  current_min_price_so_far = prices[0]
+  max_profit_so_far = prices[1]
+  #First buy
+  #Then sell
+  #Return the max profit that can be made from a single buy and sell
+
   pass
 
 
