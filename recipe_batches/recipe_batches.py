8636865bb73b24a2096c50098385dc65ba345382
fritzgt <tuanis@live.com>
2020-02-07 05:49:33 +0200

    working on recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,34 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    total = 0
+    notEnough = False
+    index = 0
+    recipe_len = len(recipe) - 1
+    for i in recipe:
+        # Check if all the needed ingredients exist
+        if i in ingredients:
+            if recipe[i] > ingredients[i]:
+                print(f"Not enough {i}")
+                notEnough = True
+            elif index == recipe_len:
+
+                total += 1
+            else:
+                ingredients[i] -= recipe[i]
+
+        else:
+            return total
+        index += 1
+    return total
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
